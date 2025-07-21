59d3ec15cfb6888cf0ef7e169168bbf2aec5e9ec
sathi <sathi@Gokularani.mshome.net>
2025-07-21 16:07:35 +0530

    This code consists of the customer detail program with separate classes

--- c2tc_javaprograms/src/day1/entity/Customer.java
+++ c2tc_javaprograms/src/day1/entity/Customer.java
@@ -1,8 +1,31 @@
 package day1.entity;
 
 public class Customer {
-	int cid;
-	String cname;
-	String city;
-
+	private int cid;
+	private String cname;
+	private String city;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Customer [cid=" + cid + ", cname=" + cname + ", city=" + city + "]";
+	}
+	
+	
 }
--- c2tc_javaprograms/src/day1/entity/CustomerDemo.java
+++ c2tc_javaprograms/src/day1/entity/CustomerDemo.java
@@ -3,24 +3,33 @@ package day1.entity;
 public class CustomerDemo {
 
 	public static void main(String[] args) {
-		int m,n;
-		m=100;
 		
 		Customer c1= new Customer();
-		c1.cid=101;
-		c1.cname="Charm";
-		c1.city="Puducherry";
-		System.out.println("Customer id:"+c1.cid);
-		System.out.println("Customer name:"+c1.cname);
-		System.out.println("Customer city:"+c1.city);
+		c1.setCid(101);
+		c1.setCname("Charm");
+		c1.setCity("Puducherry");
+		System.out.println(c1); //obj printing
+//		System.out.println("Customer id:"+c1.getCid());
+//		System.out.println("Customer name:"+c1.getCname());
+//		System.out.println("Customer city:"+c1.getCity());
 		
 		Customer c2= new Customer();
-		c2.cid=102;
-		c2.cname="Sanjuu";
-		c2.city="Chennai";
-		System.out.println("Customer id:"+c2.cid);
-		System.out.println("Customer name:"+c2.cname);
-		System.out.println("Customer city:"+c2.city);
+		c2.setCid(102);
+		c2.setCname("Sanjuu");
+		c2.setCity("Chennai");
+		System.out.println(c2);
+		
+		Customer c3= new Customer();
+		c3.setCid(103);
+		c3.setCname("Sri");
+		c3.setCity("Cuddalore");
+		System.out.println(c3); 
+		
+		Customer c4= new Customer();
+		c4.setCid(104);
+		c4.setCname("Sathiya");
+		c4.setCity("Puducherry");
+		System.out.println(c4); 
 
 	}
 
