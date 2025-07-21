2230dc9fdbfd5f8a676f6bf1e598dcc5e1071517
sathi <sathi@Gokularani>
2025-07-21 19:04:47 +0530

    This program demonstrates the valid use of identifiers

---
+++ c2tc_javaprograms/src/day1/one/IdentifersDemo.java
@@ -0,0 +1,11 @@
+package day1.one;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		int $number = 27;
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Sathiya"; 
+		System.out.println("value is : "+ studentName);
+	}
+}
