49b4fd3b1827d1c033e6e154888b2141431063b8
sathi <sathi@Gokularani>
2025-07-21 19:23:14 +0530

    This program has for loop with break statement

--- c2tc_javaprograms/src/day1/two/BreakDemo.java
+++ c2tc_javaprograms/src/day1/two/BreakDemo.java
@@ -3,7 +3,11 @@ package day1.two;
 public class BreakDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i = 4; i < 9; i++) {
+		    if (i == 6)
+		        break;
+		    System.out.println(i);
+		}
 
 	}
 
