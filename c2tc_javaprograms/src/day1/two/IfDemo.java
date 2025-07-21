4a5b003b1af1fd0ade079f8087c39efae52d7e56
sathi <sathi@Gokularani>
2025-07-21 20:13:15 +0530

    This program uses the if statement

---
+++ c2tc_javaprograms/src/day1/two/IfDemo.java
@@ -0,0 +1,15 @@
+package day1.two;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int x = 5;
+		if (x != 5) {
+		    System.out.println("Value of x is NOT 5");
+		} else {
+		    System.out.println("Value of x is 5");
+		}
+
+	}
+
+}
