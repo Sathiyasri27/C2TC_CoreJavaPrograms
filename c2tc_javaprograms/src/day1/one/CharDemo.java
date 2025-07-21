38af6864636a863d63dd1c573e0a0c1721798aef
sathi <sathi@Gokularani>
2025-07-21 18:53:13 +0530

    This program demonstrates different ways to use and represent the char data type

---
+++ c2tc_javaprograms/src/day1/one/CharDemo.java
@@ -0,0 +1,20 @@
+package day1.one;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		char ch1 = 65;
+		System.out.println(ch1);
+		char var1 = '\u00A7';
+		System.out.println(var1);
+		int a = 'A';
+		System.out.println(a);
+		char ch = 'a';
+		System.out.println(ch);
+		char var2 = '\u20AC';
+		System.out.println(var2);
+		
+
+	}
+
+}
