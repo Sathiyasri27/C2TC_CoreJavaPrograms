1b9aa4b8e15ef7c09072440186ce11fd0a9fcfeb
sathi <sathi@Gokularani>
2025-07-22 13:38:12 +0530

    This program has 4 product details

---
+++ c2tc_javaprograms/src/entityclassassignment2/Product.java
@@ -0,0 +1,33 @@
+package entityclassassignment2;
+
+public class Product {
+
+	public static void main(String[] args) {
+		ProductDemo p1= new ProductDemo();
+		p1.setPid(101);
+		p1.setPname("XXX");
+		p1.setPrice("rs.10");
+		System.out.println(p1);
+		
+		ProductDemo p2= new ProductDemo();
+		p2.setPid(102);
+		p2.setPname("YYY");
+		p2.setPrice("rs.10");
+		System.out.println(p2);
+		
+		ProductDemo p3= new ProductDemo();
+		p3.setPid(103);
+		p3.setPname("ZZZ");
+		p3.setPrice("rs.10");
+		System.out.println(p3);
+		
+		ProductDemo p4= new ProductDemo();
+		p4.setPid(104);
+		p4.setPname("XYZ");
+		p4.setPrice("rs.10");
+		System.out.println(p4);
+		
+
+	}
+
+}
