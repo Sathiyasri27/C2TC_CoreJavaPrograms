2d2fc90bf121061a0807ebaa42169ee558de40f8
sathi <sathi@Gokularani>
2025-07-23 18:31:35 +0530

    This is salary details program

---
+++ c2tc_javaprograms/src/assignment2/CommissionDemo.java
@@ -0,0 +1,11 @@
+package assignment2;
+
+public class CommissionDemo {
+	public static void main(String[] args) {
+		
+        Commission obj = new Commission();
+        obj.acceptDetails();
+        obj.calculateCommission();
+    }
+
+}
