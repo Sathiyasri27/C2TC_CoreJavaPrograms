4076f60d8d166650799ad7791b87ad6d6cd5d686
sathi <sathi@192.168.155.123>
2025-07-24 23:48:02 +0530

    This is a single inheritance student details program

--- c2tc_javaprograms/src/day5/multilevelinheritance/vehicle/Car.java
+++ c2tc_javaprograms/src/day5/multilevelinheritance/vehicle/Car.java
@@ -1,7 +1,7 @@
 package day5.multilevelinheritance.vehicle;
 
-public class car {
-	public car() {
+public class Car {
+	public Car() {
 		System.out.println("Class Car");
 	}
 
---
+++ c2tc_javaprograms/src/day5/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,10 @@
+package day5.singleinheritance;
+
+public class SLInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
