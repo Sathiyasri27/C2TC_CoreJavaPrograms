70af1ad6085aa3c65e8bd9d9bb67424945162c8b
sathi <sathi@Gokularani.mshome.net>
2025-07-24 13:57:19 +0530

    This program display the address

---
+++ c2tc_javaprograms/src/day5/association/hasa/Exceutor.java
@@ -0,0 +1,12 @@
+package day5.association.hasa;
+
+public class Exceutor {
+
+	public static void main(String[] args) {
+		Address address = new Address("405 Laxmi Enclave, Pandit Colony", "Nashik", "Maharashtra", "422003");
+		Person person = new Person("Sathiya", address);
+		person.displayInfo();
+
+	}
+
+}
