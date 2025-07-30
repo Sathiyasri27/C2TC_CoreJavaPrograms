3990a4f84142c81e1362e3df8272616593d2bf17
Sathiyasri27 <sathiyasri0027@gmail.com>
2025-07-30 19:12:23 +0530

    This code contains the ticket booking program

---
+++ c2tc_javaprograms/src/Assignment5/TicketBooking.java
@@ -0,0 +1,54 @@
+package Assignment5;
+
+public class TicketBooking {
+    private String stageEvent;
+    private String customer;
+    private Integer noOfSeats;
+
+    public TicketBooking() {}
+    public TicketBooking(String stageEvent, String customer, Integer noOfSeats) {
+        this.stageEvent = stageEvent;
+        this.customer = customer;
+        this.noOfSeats = noOfSeats;
+    }
+    public String getStageEvent() {
+        return stageEvent;
+    }
+
+    public void setStageEvent(String stageEvent) {
+        this.stageEvent = stageEvent;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public Integer getNoOfSeats() {
+        return noOfSeats;
+    }
+
+    public void setNoOfSeats(Integer noOfSeats) {
+        this.noOfSeats = noOfSeats;
+    }
+
+    //Cash Method
+    public void makePayment(double amount) {
+        System.out.printf("Amount %.1f paid in cash\n", amount);
+    }
+
+    //Wallet Method
+    public void makePayment(double amount, String walletNumber) {
+        System.out.printf("Amount %.1f paid using wallet number %s\n", amount, walletNumber);
+    }
+
+    //Credit Card Method
+    public void makePayment(String holderName, double amount, String creditCardType, String ccv) {
+        System.out.println("Holder name:" + holderName);
+        System.out.printf("Amount %.1f paid using %s card\n", amount, creditCardType);
+        System.out.println("CCV:" + ccv);
+    }
+}
