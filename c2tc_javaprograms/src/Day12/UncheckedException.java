1cdd566b7069e0bc99951fc9a310ff4dea1a3169
Sathiyasri27 <sathiyasri0027@gmail.com>
2025-07-31 16:04:39 +0530

    This is exception program

---
+++ c2tc_javaprograms/src/Day12/UncheckedException.java
@@ -0,0 +1,12 @@
+package Day12;
+
+public class UncheckedException {
+
+	public static void main(String[] args) {
+		int intArray[];
+			intArray = new int[] { 1, 2, 3, 4 };
+			System.out.println(intArray[6]);
+
+	}
+
+}
---
+++ c2tc_javaprograms/src/Day12/uerdefinedexp/InvalidMarksException.java
@@ -0,0 +1,12 @@
+package Day12.uerdefinedexp;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+}
